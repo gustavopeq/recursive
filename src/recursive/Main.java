272a49061ca7165988972334e271c81432889729
gustavopeq <gustavo_pequeno2@hotmail.com>
2020-04-08 19:31:34 -0700

    Sum array elements recursively

--- src/recursive/Main.java
+++ src/recursive/Main.java
@@ -3,8 +3,34 @@ package recursive;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Object[] array3 = {-10,-10};
+		Object[] array2 = {4,1,array3};
+		Object[] array = {1,4, array2 ,5};
+		
+		System.out.println(sumArray(1,array));
 	}
+	
+	public static int sumArray(int depth, Object[] array) 
+	{
+		int sum = 0;
+		for (Object element : array) 
+		{ 
+			if(element instanceof Integer) 
+			{
+				sum = sum + ((int)element*depth);
+			}else 
+			{
+				sum = sum + sumArray(depth + 1, (Object[])element);
+			}
+		}
+		
+		return sum;
+	}
+	
+	
+	
+	
+	
 
 }
